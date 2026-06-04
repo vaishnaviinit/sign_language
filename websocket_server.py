99154658a73478aa0b12947ba12d8ec009ea7ed7
vaishnaviinit <vaishnavipixy@gmail.com>
2026-06-04 13:52:15 +0530

    feat: add Socket.IO room-based chat backend

--- websocket_server.py
+++ websocket_server.py
@@ -1,5 +1,5 @@
 from flask import Flask 
-from flask_socketio import SocketIO, emit
+from flask_socketio import (SocketIO, emit,join_room,leave_room )
 
 
 app = Flask(__name__)
@@ -38,20 +38,46 @@ def handle_connect():
 def handle_disconnect():
     print("Client disconnected")
 
-def handle_chat(data):
-    print("recieved: ", data)        
+
+
+def handle_join_room(data):
+    username=data["username"]
+    room=data["room"]
+    join_room(room)
+   
+    print(f"{username} joined {room}")
+
     emit(
-        "chat_message",
-        data,
-        broadcast=True  
+        "system_message",
+        {
+            "message": f"{username} joined the room {room}"
+        },
+        room=room
     )
+
+def handle_send_message(data):
+    username=data["username"]
+    room=data["room"]
+    message=data["message"]
+   
+    print(f"[{room}]{username}: {message}")
+
+    emit(
+        "recieve_message",
+        {
+            "username": username,
+            "message": message
+        },
+        room=room
+    )
+
 
 #register websocket events manually
 
 socketio.on_event("connect", handle_connect)
 socketio.on_event("disconnect", handle_disconnect)
-socketio.on_event("chat", handle_chat)
-
+socketio.on_event("join_room", handle_join_room)
+socketio.on_event("send_message", handle_send_message)
 
 #start the server
 
